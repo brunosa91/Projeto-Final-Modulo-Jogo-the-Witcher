e0b2db559244852190de580034acdb32eda3120c
brunosa91 <brunosantana.engenharia@gmail.com>
2021-11-20 12:43:24 -0300

    test - desenvolvida lógica com estrutura de repetição para segunda fase do jogo

---
+++ Java script/segunda fase geralt.js
@@ -0,0 +1,20 @@
+function segundaFase (){
+
+    var question = (prompt ("Responda exatamente o número, ex: 404"))
+    
+    while (question <= 3){
+    
+    if (question === 3){
+       
+        window.location.href = "terceira-fase-geralt.html"
+       
+        
+    }else{
+        
+        alert("Resposta errada!\nNúmero errado de nilfigardianos")
+    }
+}   
+    
+    }
+    
+    
