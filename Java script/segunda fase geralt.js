97f6fbe1d80c183cd03fb55e13a856d53eb984d3
brunosa91 <brunosantana.engenharia@gmail.com>
2021-11-20 14:24:09 -0300

    feat - realiza o código java script da página html  segunda fase geralt

--- Java script/segunda fase geralt.js
+++ Java script/segunda fase geralt.js
@@ -1,16 +1,42 @@
 function segundaFase (){
+    var cont = 0
+    var tent = 1
+    while(cont <= 3 ){
 
-    var question = (prompt ("Responda exatamente o número, ex: 404"))
+        var question = (prompt ("Responda exatamente o número, ex: 404"))
     
     
-    if (question == 3){
+        if (question == 3){
        
-        window.location.href = "terceira fase geralt.html"
+            window.location.href = "terceira fase geralt.html"
+
+        break;
        
         
-    }else{
+        }
+
+        else if(tent < 3)
+            {
+            
+            alert("Resposta errada!\nNúmero errado de nilfigardianos")
+
         
-        alert("Resposta errada!\nNúmero errado de nilfigardianos")
+            }
+
+        else
+            {
+
+            alert("game over")
+            break;
+            
+            }
+
+
+        cont ++
+        tent ++
     }
+    
 }
+
+
     
---
+++ Java script/terceira fase geralt.js
@@ -0,0 +1,22 @@
+function terceiraFase (){
+
+    var question = (prompt (" 1 - Ajudar a Ciri?\n 2 - Ajudar Yennefer?"))
+    
+    
+    
+    if (question == 1){
+       
+        window.location.href = ""
+       
+    }else if (question == 2){
+        
+         window.location.href = "Final-Geral.html"
+        
+    }else{
+        
+        alert("digite 1 ou 2")
+    }
+    
+    }
+    
+    
