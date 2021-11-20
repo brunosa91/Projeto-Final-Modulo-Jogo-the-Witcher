f158da265fbb4af259d5514410634781a654a737
brunosa91 <brunosantana.engenharia@gmail.com>
2021-11-20 12:59:52 -0300

    test-adicionado na página html da segunda fase o arquivo java script sem a lógica de repetição

--- Java script/segunda fase geralt.js
+++ Java script/segunda fase geralt.js
@@ -2,19 +2,15 @@ function segundaFase (){
 
     var question = (prompt ("Responda exatamente o número, ex: 404"))
     
-    while (question <= 3){
     
-    if (question === 3){
+    if (question == 3){
        
-        window.location.href = "terceira-fase-geralt.html"
+        window.location.href = "terceira fase geralt.html"
        
         
     }else{
         
         alert("Resposta errada!\nNúmero errado de nilfigardianos")
     }
-}   
-    
-    }
-    
+}
     
