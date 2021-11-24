a86c67cf76bbe0114e98580d9e3fb230b9ec8385
brunosa91 <brunosantana.engenharia@gmail.com>
2021-11-24 01:34:11 -0300

    Feat - Criado os arquivos de java scrip do personagem dandelion e colocado o enredo atualizado nos game over

---
+++ Java script/terceira fase geralt dandelion.js
@@ -0,0 +1,22 @@
+function terceiraFase (){
+
+    var question = (prompt (" 1 - Ajudar a Ciri?\n 2 - Ajudar Yennefer?"))
+    
+    
+    
+    if (question == 1){
+       
+        window.location.href = "Geralt-game over fase 3.html"
+       
+    }else if (question == 2){
+        
+         window.location.href = "Geralt-Final.html"
+        
+    }else{
+        
+        alert("digite 1 ou 2")
+    }
+    
+    }
+    
+    
