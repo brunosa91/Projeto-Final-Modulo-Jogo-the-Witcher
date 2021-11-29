226210a69f27ef1b04c98f940eb0512ebdb25120
brunosa91 <brunosantana.engenharia@gmail.com>
2021-11-28 21:48:49 -0300

    alteração no js fase 1 geralt e no footer da fase 2 geralt

--- Java script/primeira fase geralt.js
+++ Java script/primeira fase geralt.js
@@ -1,6 +1,6 @@
 function PrimeiraFase (){
 
-var question = (prompt ("Fase 1- Gerald observa uma chance de atacar radovid, mas ciri está em perigo contra 3 soldados de rendânia. O que vc faz?defender o que tem mais de precioso tomando as decisões corretas nessa guerra.\n 1 - Ajudar a Ciri?\n 2 - Tentar capturar Radovid?"))
+var question = (prompt ("1 - Ajudar a Ciri?\n2 - Tentar capturar Radovid?"))
 
 
 
